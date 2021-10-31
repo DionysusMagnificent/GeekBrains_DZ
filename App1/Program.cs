68bae9f45a6262c943749cef766ed46fc78b672f
DionysusMagnificent <80987273+DionysusMagnificent@users.noreply.github.com>
2021-10-31 17:39:48 +0200

    Add files via upload

---
+++ App1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как тебя зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет {name}! сегодня  {DateTime.Now}");
+            Console.ReadKey();
+
+        }
+    }
+}
